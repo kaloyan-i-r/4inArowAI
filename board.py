392027a40ec666c249d9a7243f9284e452385563
kaloyan-i-r <kaloyan.i.r@gmail.com>
2018-01-20 23:01:38 +0200

    add some changes

--- board.py
+++ board.py
@@ -1,11 +1,12 @@
 import numpy
+import math,time,copy
 from random import *
 from const import *
 
 W = 7
 H = 6
 
-def printBoard(board):
+def print_board(board):
     if board is None:
         print('no board!')
         return
@@ -13,62 +14,112 @@ def printBoard(board):
     for i in range(H):
         for j in range(W):
             value = board[0,H*W-(W-j + i*W)]
-            # print(f'value: {H*W-(W-j + i*W)}')
             symbol = symbols[str(int(value))]
             print(f'  {symbol}', end="")
         print('')
     print('-----------------------')
+def board_str(board):
+    return string_diag(board[0])
+
+def check_board_for_winner(board):
+    board_string = board_str(board)
+    lines = []
+    for i in range(H):
+        tmp_line = ''
+        for j in range(W):
+            tmp_line += symbols[str(int(board[0][j+i*W]))]
+        lines.append(tmp_line)
+    for i in range(W):
+        tmp_line = ''
+        for j in range(H):
+            tmp_line += symbols[str(int(board[0][j*W + i]))]
+        lines.append(tmp_line)
+    for line in lines:
+        if 'AAAA' in line or 'BBBB' in line:
+            raise NameError('winner :/')
 
 
-def generateBoard(moves):
-    print(f'moves: {moves}')
-    board = numpy.empty( (1,H*W,))
+def increment_board(board):
+    player = get_player_by_board(board)
+    weighted_moves = get_weighted_moves(board, True)
+    boards_increment = []
+    if len(weighted_moves.keys()) > 0:
+        optimal_move_value = max(weighted_moves.values())
+        optimal_positions = []
+        for position, weight in weighted_moves.items():
+            if weight == optimal_move_value:
+                optimal_positions.append(position)
+        for move in optimal_positions:
+            new_board = copy.copy(board)
+            apply_move(new_board, move, player)
+            try:
+                check_board_for_winner(new_board)
+            except NameError:
+                weighted_moves = get_weighted_moves(board, True, True)
+                print(f'player:{player} / move:{move} / ')
+                print(weighted_moves)
+                print(move)
+                print_board(board)
+                print_board(new_board)
+                raise NameError('end')
+
+            boards_increment.append(new_board)
+    else:
+        return None
+    return boards_increment
+
+def generate_board(moves):
+    board = numpy.empty( (1, H*W,))
     board[:] = 0
     for i in range(moves):
-        player = getPlayer(i)
-        possible_moves = getPossibleMoves(board)
-        not_winning_moves = filterWinningMoves(player,board,possible_moves)
-        weighted_moves = evaluateMoves(board,not_winning_moves,player)
-        print(f'player: {symbols[str(int(player))]} {weighted_moves}')
-        printBoard(board)
-        if len(not_winning_moves) > 0:
-            move = sample(not_winning_moves,1)[0]
-            applyMove(board,move,player)
+        player = get_player(i)
+        possible_moves = get_possible_moves(board)
+        weighted_moves = evaluate_moves(board,possible_moves,player)
+        if len(weighted_moves.keys()) > 0:
+            optimal_move_value = max(weighted_moves.values())
+            optimal_positions = []
+            for position, weight in weighted_moves.items():
+                if weight == optimal_move_value:
+                    optimal_positions.append(position)
+            move = sample(optimal_positions,1)[0]
+            apply_move(board,move,player)
         else:
-            print(f'{i} p/nw : {not_winning_moves}/{possible_moves}')
-            # printBoard(board)
             return None
-        # printBoard(board)
     return board
 
-def applyMove(board,move,player):
-    board[0,findPosition(board,move)] = player
+def apply_move(board,move,player):
+    board[0,find_position(board,move)] = player
     return board
 
-def getPossibleMoves(board):
+
+def get_possible_moves(board):
     moves = []
     for i in range(7):
-        # print(f'move: {i} / possition: {findPosition(board,i)}')
-        if findPosition(board,i) != None:
+        if find_position(board,i) is not None:
             moves.append(i)
     return moves
 
-def filterWinningMoves(player,board,moves):
+
+def filter_winning_moves(player, board, moves, debug=False):
     filtered_moves = []
     for move in moves:
-        if not checkIfWinningMove(player,board,move):
+        if not check_if_winning_move(player, board, move, debug):
             filtered_moves.append(move)
     return filtered_moves
 
-def checkIfWinningMove(player,board,move):
-    diagonals = generateDiagonals(board,move)
-    for diagonal in diagonals:
-        str_diag = stringDiag(diagonal)
-        if symbols[str(player)]*3 in str_diag:
+
+def check_if_winning_move(player, board, move, debug=False):
+    calculated_diagonals = generate_diagonals(board,move)
+    if debug: print(f'move: {move} / calculated diagonals: {calculated_diagonals}')
+    for diagonal in calculated_diagonals:
+        str_diag = string_diag(diagonal, player)
+        if debug: print(f'str_diag: {str_diag}')
+        if symbols[str(abs(player))]*3 in str_diag:
             return True
     return False
 
-def findPosition(board,move):
+
+def find_position(board,move):
     for i in range(H):
         position = move + W*i
         pos_value = board[0,position]
@@ -76,50 +127,119 @@ def findPosition(board,move):
             return position
     return None
 
-def evaluateMoves(board,moves,player):
+
+def evaluate_moves(board,moves,player):
     weighted_moves = {}
     for move in moves:
         weighted_moves[move] = cost(board,move,player)
     return weighted_moves
 
+
 def cost(board,move,player):
-    diagonals = generateDiagonals(board,move)
+    diagonals = generate_diagonals(board,move)
     weights = []
     for diag in diagonals:
-        weights.append(surrounding_options[stringDiag(diag,player)])
-    return max(weights)
+        # print(string_diag(diag))
+        weights.append(surrounding_options[string_diag(diag,player)])
+    return sum(weights)
 
-def generateDiagonals(board,move):
-    position = findPosition(board,move)
-    x,y = move % 7,move // 7
+
+def get_player_by_board(board):
+    empty_spaces = (board[0] == 0).sum()
+    current_move = H * W - empty_spaces
+    return get_player(current_move)
+
+
+def get_weighted_moves(board, filter_winning=False, debug=False):
+    empty_spaces = (board == 0).sum()
+    current_move = H * W - empty_spaces + 1
+    player = get_player_by_board(board)
+    possible_moves = get_possible_moves(board)
+    if debug: print(f'possible_moves: {possible_moves}')
+    if filter_winning:
+        possible_moves = filter_winning_moves(player, board, possible_moves, debug)
+    if debug: print(f'filtered_possible_moves: {possible_moves}')
+    return evaluate_moves(board, possible_moves, player)
+
+
+def generate_diagonals(board,move):
+    position = find_position(board,move)
+    x,y = position % 7,position // 7
     correct_neighbours = []
     for diagonal in diagonals:
         temp_neighbour = []
         for x_delta,y_delta in diagonal:
             temp_x,temp_y = x_delta + x, y_delta +y
-            if temp_x < 0 or W <= temp_y:
-                temp_neighbour.append(-2)
-            elif temp_y < 0 or H <= temp_y:
+            temp_position = temp_x+temp_y*W
+            if temp_x < 0 or W <= temp_x or temp_y < 0 or H <= temp_y or temp_position < 0 or temp_position >= (H*W):
                 temp_neighbour.append(-2)
             else:
-                temp_neighbour.append(board[0,int(temp_x+temp_y*W)])
+                temp_neighbour.append(board[0,int(temp_position)])
         correct_neighbours.append(temp_neighbour)
     return correct_neighbours
 
-def getPlayer(move_number):
+def get_player(move_number):
     return -1 if move_number % 2 == 1 else 1
 
+def vectorized_moves(moves):
+    max_weight = max(moves.values())
+    print(max_weight)
+    vect_moves = []
+    for i in range(W):
+        if i in moves and moves[i] == max_weight:
+            vect_moves.append(1)
+        else:
+            vect_moves.append(0)
+    return numpy.array(vect_moves)
+
+
+def generate_boards(moves):
+    boards = {}
+    empty_board = generate_board(0)
+    boards[board_str(empty_board)] = empty_board
+    count_all = 0
+    prev_tier_boards = [empty_board]
+    cur_tear_boards = []
+    for i in range(1, moves+1):
+        start_time = time.time()
+        count_collisions = 0
+        # print(prev_tier_boards)
+        # sample_number = int(math.sqrt(len(prev_tier_boards)))
+        # print(sample_number)
+        # sample_boards = sample(prev_tier_boards, sample_number)
+        # for board in sample_boards:
+        for board in prev_tier_boards:
+            temp_tear_boards = increment_board(board)
+            for new_board in temp_tear_boards:
+                board_string = board_str(new_board)
+                count_all += 1
+                if board_string in boards:
+                    count_collisions += 1
+                else:
+                    boards[board_string] = new_board
+                    # if count_collisions > len(cur_tear_boards) / i**3:
+                    #     break
+            cur_tear_boards.extend(temp_tear_boards)
+        print(f'moves: {i} :: generated/max: {len(cur_tear_boards)}/{len(prev_tier_boards)*7} time : {time.time() - start_time}')
+        prev_tier_boards = cur_tear_boards
+        cur_tear_boards = []
+    print(f'all_boards : {len(boards)}')
+    return boards
+
 def main():
-    # printBoard(generateBoard(0))
-    # printBoard(generateBoard(5))
-    # printBoard(generateBoard(10))
-    # printBoard(generateBoard(15))
-    # printBoard(generateBoard(20))
-    # printBoard(generateBoard(25))
-    # printBoard(generateBoard(30))
-    printBoard(generateBoard(35))
-    # printBoard(generateBoard(43))
-    pass
+    gen_boards = generate_boards(36)
+    training_set = []
+    for str_board in gen_boards:
+        board = gen_boards[str_board]
+        weighted_moves = get_weighted_moves(board)
+        training_set.append([board,vectorized_moves(weighted_moves)])
+        print(weighted_moves)
+        print_board(board)
+    print(len(training_set))
+    import pickle
+    with open("training_set_data",'wb') as set_file:
+        pickle.dump(training_set,set_file)
+        pass
 
 if __name__ == '__main__':
     main()
--- const.py
+++ const.py
@@ -10,7 +10,7 @@ import re
 diagonals = [
     [[-3,0],[-2,0],[-1,0],[1,0],[2,0],[3,0]],
     [[0,-3],[0,-2],[0,-1],[0,1],[0,2],[0,3]],
-    [[-3,-3],[-2,-2],[-1,1],[1,1],[2,2],[3,3]],
+    [[-3,-3],[-2,-2],[-1,-1],[1,1],[2,2],[3,3]],
     [[3,-3],[2,-2],[1,-1],[-1,1],[-2,2],[-3,3]]
 ]
 
@@ -20,15 +20,15 @@ symbols = {"1" :'A',
            "0" :'_',
            "-2":'X'}
 
-def stringDiag(diag,player=1):
+def string_diag(diag,player=1):
     str_diag = ''
     for x in diag:
-        if x == str(player):
-            str_diag+= symbols[abs(player)]
-        elif x == str(player*-1):
-            str_diag+= symbols[-abs(player)]
+        if x == player:
+            str_diag += symbols[str(abs(int(player)))]
+        elif x == player*-1:
+            str_diag += symbols[str(-abs(int(player)))]
         else:
-            str_diag+= symbols[str(int(x))]
+            str_diag += symbols[str(int(x))]
         # print(f'player; {player} / x:{x} / str_diag:{str_diag}')
     return str_diag
 
@@ -41,6 +41,8 @@ def symbolsToNumbers(string_val):
 def calculateOptions():
     temp_optins = {}
     keys = symbols.values()
+    win_weight = 100
+    weight_divider = 4
     for one in keys:
         for two in keys:
             for three in keys:
@@ -50,38 +52,41 @@ def calculateOptions():
                             str_val = f'{one}{two}{three}{four}{five}{six}'
                             str_weight = 0
                             if 'AAA' in str_val:
-                                str_weight = 100
+                                str_weight = 0#100
                             elif 'BBB' in str_val:
-                                str_weight = 80
+                                str_weight = 2#80/25
                             elif re.match('.*[AB_]X+[AB_].*', str_val):
                                 continue
                             elif re.match('^X[AB_]X.*', str_val) or re.match('.*X[AB_]X$',str_val):
                                 continue
                             elif re.match('[AB_]{1,2}X.*',str_val) or re.match('.*X[AB_]{1,2}$',str_val):
                                 continue
-                            elif re.match('.+A{2}.+', str_val) or re.match('AA_.*', str_val) or re.match('.*_AA', str_val): str_weight = 50
+                            elif re.match('.+A{2}.+', str_val) or re.match('AA_.*', str_val) or re.match('.*_AA', str_val):
+                                str_weight = 3#50
                             elif re.match('.+B{2}.+', str_val) or re.match('BB_.*', str_val) or re.match('.*_BB', str_val):
-                                str_weight = 40
+                                str_weight = 4#40/12,5
                             elif re.match('.*X[AB_]{2}X.*', str_val):
-                                str_weight = 1
+                                str_weight = 9#1
                             elif re.match('.*X_{3,4}X.*', str_val):
-                                str_weight = 15
+                                str_weight = 7#15
+                            elif re.match('______', str_val):
+                                str_weight = 7#40 ?????
                             elif re.match('.*A[_]{3}.*', str_val) or re.match('.*[_]{3}A.*', str_val):
-                                str_weight = 30
+                                str_weight = 5#30/6,5
                             elif re.match('.*[A_]{3,4}.*', str_val) or re.match('.*[A_]{3,4}.*', str_val):
-                                str_weight = 30
+                                str_weight = 5#30/6.5
                             elif re.match('.*BABAB.*', str_val) or re.match('.*ABABB.*', str_val) or re.match('.*BBABA.*', str_val):
-                                str_weight = 1
+                                str_weight = 9#1
                             elif re.match('.*B_B.*', str_val):
-                                str_weight = 25
+                                str_weight = 5#25/
                             elif re.match('.*B__.*', str_val) or re.match('.*__B.*',str_val) or  re.match('.*_B_.*',str_val):
-                                str_weight = 10
+                                str_weight = 8#10/
                             elif re.match('.*X[AB_]{3}X.*', str_val):
-                                str_weight = 1
+                                str_weight = 9#1
                             else:
-                                str_weight = 1
+                                str_weight = 9#1
                                 pass
-                            temp_optins[str_val] = str_weight
+                            temp_optins[str_val] = win_weight/(weight_divider**str_weight)
     return temp_optins
 
 surrounding_options = calculateOptions()
